1f4a446ac51298831274725d06fb1e4c519c0f50
LORDNR <ratchanons.work@gmail.com>
2023-08-18 15:43:53 +0700

    feat update and delete in Attraction

--- src/api/attraction/controllers/attraction.ts
+++ src/api/attraction/controllers/attraction.ts
@@ -33,5 +33,44 @@ export default factories.createCoreController('api::attraction.attraction', ({ s
 
     const arrayResponse = await contentAPI.output(attraction, contentType, ctx.state.auth);
     return arrayResponse[0];
+  },
+
+  async update(ctx) {
+    try {
+      const findOneAttraction = await this.findOne(ctx)
+
+      if (!findOneAttraction) return
+
+      const id = findOneAttraction.id
+      const { body } = ctx.request;
+      body.updatedAt = new Date()
+
+      await strapi.entityService.update("api::attraction.attraction", id, body)
+
+      ctx.query = {
+        populate: '*'
+      }
+
+      const returnFindOneAttraction = await this.findOne(ctx)
+
+      return returnFindOneAttraction
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  async delete(ctx) {
+    try {
+      const findOneAttraction = await this.findOne(ctx)
+
+      if (!findOneAttraction) return
+
+      const id = findOneAttraction.id
+      await strapi.entityService.delete("api::attraction.attraction", id)
+
+      ctx.send({}, 204)
+    } catch (err) {
+      console.log(err);
+    }
   }
 }))
